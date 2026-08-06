d17384daf7b0423067384ff02dd148f3fc543d64
Dhanushraagav <dhanushraagav01@gmail.com>
2026-08-06 15:28:50 +0530

    style: refine brand header logo to clean static ALL CAPS presentation with equal height emblems and crisp solid colors

--- client/js/auth.js
+++ client/js/auth.js
@@ -1603,7 +1603,7 @@ function updateAuthUI() {
             <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Emblem_of_Tamil_Nadu.svg" alt="TN Emblem" class="brand-emblem-tn" />
           </div>
           <div class="brand-text-container">
-            <span class="brand-title">CROWDCITY AI</span>
+            <span class="brand-title">CROWDCITY</span>
             <span class="brand-subtitle">CITIZEN PORTAL</span>
           </div>
         </a>
