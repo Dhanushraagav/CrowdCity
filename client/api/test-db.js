4ea879796f9a416ca646d9c2dcab91d375048dcb
Dhanushraagav <dhanushraagav01@gmail.com>
2026-08-29 19:22:19 +0530

    fix: use CommonJS format for Vercel functions

--- client/api/test-db.js
+++ client/api/test-db.js
@@ -1,4 +1,4 @@
-﻿export default async function handler(req, res) {
+module.exports = async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   try {
     const supabaseUrl = 'https://swbktcwlxbnbsjrmmmjj.supabase.co';
@@ -17,4 +17,4 @@
   } catch (err) {
     return res.status(500).json({ ok: false, error: err.message });
   }
-}
+};
